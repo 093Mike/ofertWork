17b4a531c6ee2e545abb2a86b6d2027fa1325d5f
Sackmike <sackmike.093@gmail.com>
2022-06-15 19:42:32 +0200

    0.06 Update Rutes

--- src/views/profile/profile.js
+++ src/views/profile/profile.js
@@ -1,22 +1,8 @@
 import styled from '@emotion/styled';
-import Footer from '../elements/footer';
-import Header from '../elements/header';
 
-let Log = styled.div`
-    background-color:#fff;
-    width: 60%;
-    height: 20em;
-    box-shadow: 0px 0px 0px 1px rgb(32 32 32 / 25%);
-    transition: 0.5s;
-    text-align:center;
-    display:flex;
-    flex-direction:column;
-    &:hover{
-        box-shadow: 10px 7px 18px 1px rgb(32 32 32 / 75%)
-    }
-`
 
-function Login(props) {
+
+function Pro(props) {
     return (
         <>
             <Header colores={props.colores} changeMode={props.changeMode} />

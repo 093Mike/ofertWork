ad457da6248e40da5357b38f1406af343140bb58
Sackmike <sackmike.093@gmail.com>
2022-06-15 20:04:05 +0200

    0.07 Login Update

--- src/views/logIn/logIn.js
+++ src/views/logIn/logIn.js
@@ -1,33 +1,74 @@
 import styled from '@emotion/styled';
 import Footer from '../elements/footer';
 import Header from '../elements/header';
+import imgFondo from '../../img/foto1.jpg'
+
+let LogInSection = styled.section`
+    height: 90vh;
+    background-image: url(${imgFondo});
+    display: flex;
+    justify-content: center;
+    align-content: left;
+    flex-direction: column;
+    align-items: left;
+    padding: 0 20em;
+`
+
+
+let H1C = styled.h1`
+    font-size:40px;
+`
 
 let Log = styled.div`
     background-color:#fff;
-    width: 60%;
-    height: 20em;
+    width: 30em;
+    height: auto;
     box-shadow: 0px 0px 0px 1px rgb(32 32 32 / 25%);
     transition: 0.5s;
-    text-align:center;
+    text-align:left;
     display:flex;
     flex-direction:column;
+    padding:20px;
     &:hover{
         box-shadow: 10px 7px 18px 1px rgb(32 32 32 / 75%)
     }
 `
+let Labels = styled.label`
+
+`
+
+let Input = styled.input`
+    width:70%;
+    font-size:18px;
+    margin: 0 0 5px;
+`
+
+
+
 
 function Login(props) {
+
+    let Submit = styled.div`
+        width:6em;
+        margin:20px 0;
+        padding:10px;
+        color:${props.colores.colorText1} ;
+        background-color:${props.colores.color1}
+    `
+
     return (
         <>
             <Header colores={props.colores} changeMode={props.changeMode} />
-            <section id="logIn">
+            <LogInSection>
+                <H1C>Inicia sesión</H1C>
                 <Log>
-                    <h1>Log In</h1>
-                    <input></input>
-                    <input></input>
-                    <div></div>
+                    <Labels>Correo</Labels>
+                    <Input type={"email"}></Input>
+                    <Labels>Contraseña</Labels>
+                    <Input type={"password"}></Input>
+                    <Submit>Iniciar sesión</Submit>
                 </Log>
-            </section>
+            </LogInSection>
             <Footer/>
         </>
 
